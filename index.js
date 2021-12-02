57d37f2f57439b6c39e51f5eb7426b255526b43a
Aurelien <aurelien.coste@etu.univ-grenoble-alpes.fr>
2021-12-02 23:53:28 +0100

    Depot de quelques pages

--- index.js
+++ index.js
@@ -18,6 +18,23 @@ router.get('/', async (ctx) => {
     await ctx.render('index.ejs', { message: 'test !' })
 })
 
+
+let article = {
+    title : 'Ceci est un titre',
+    img : '/img/test.png',
+    description : 'Ceci est la description de l\'article'
+}
+
+  
+
+router.get('/resultatsderecherche', async (ctx) => {
+    await ctx.render('resultatsderecherche.ejs', { filtres: ':filtres', resultats: [article] })
+})
+
+router.get('/apropos', async (ctx) => {
+    await ctx.render('apropos.ejs', { message: 'test !' })
+})
+
 app
     .use(router.routes())
     .use(router.allowedMethods)
