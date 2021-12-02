f7ad353cf1e3de3599c30daaaae06e09bebc211b
Ana Gelez <ana@gelez.xyz>
2021-12-02 20:21:25 +0100

    Layout

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Koa = require('koa')
 const Router = require('koa-router')
 const views = require('koa-views')
+const staticFiles = require('koa-static')
 
 const app = new Koa()
 const router = new Router()
@@ -11,6 +12,7 @@ const render = views(__dirname + '/views', {
 })
 
 app.use(render)
+app.use(staticFiles('public'))
 
 router.get('/', async (ctx) => {
     await ctx.render('index.ejs', { message: 'test !' })
